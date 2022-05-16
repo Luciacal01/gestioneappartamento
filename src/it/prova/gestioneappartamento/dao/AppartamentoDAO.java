411e7e491c24256d8944fd91fbe2d3ff7cf1519f
Luciacal01 <luciacalabria80@gmail.com>
2022-05-16 15:43:22 +0200

    primo commit

---
+++ src/it/prova/gestioneappartamento/dao/AppartamentoDAO.java
@@ -0,0 +1,5 @@
+package it.prova.gestioneappartamento.dao;
+
+public class AppartamentoDAO {
+
+}
---
+++ src/it/prova/gestioneappartamento/test/TestGestioneAppartamento.java
@@ -0,0 +1,5 @@
+package it.prova.gestioneappartamento.test;
+
+public class TestGestioneAppartamento {
+
+}
